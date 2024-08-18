f21d549bfad632128fd4ae47e5b4aa42d8f2990c
Roshani9731 <roshaninavdiya9731@gmail.com>
2024-08-18 18:19:43 +0530

    fix: dynamic port

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const rootRouter = require("./routes");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const port = process.env.PORT || 3100;
 
 const app = express();
 console.log("app started");
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
 app.use("/api/v1/", rootRouter);
 
 app.listen(port);
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 mongoose.connection.on("error", (err) => {
--- routes/config.js
+++ routes/config.js
@@ -1,14 +1,4 @@
-const { JWT_SECRET } = require("../config");
-
 // config.js
 module.exports = {
-  db: {
-    host: "localhost",
-    user: "root",
-    password: "password",
-  },
-  server: {
-    port: 3000,
-  },
   JWT_SECRET: "roshani",
 };
